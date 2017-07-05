e8b7952fcae8512a5c44a74fbf5143539b7c88b6
liwuyang <1299474252@qq.com>
2017-07-05 13:47:50 +0800

    first commit

---
+++ wawj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wawj
+//
+//  Created by ruiyou on 2017/7/5.
+//  Copyright © 2017年 technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wawj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wawj
+//
+//  Created by ruiyou on 2017/7/5.
+//  Copyright © 2017年 technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
